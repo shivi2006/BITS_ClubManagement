54fab034ec2fef91cbfb2bc0a528e01505928f52
shivi2006 <f20190778@pilani.bits-pilani.ac.in>
2020-01-22 20:47:35 +0530

    Project

---
+++ app/src/main/java/com/example/shiviMittal/MainActivity.kt
@@ -0,0 +1,16 @@
+
+
+package com.example.shiviMittal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomBar:BottomNavigationView=findViewById(R.id.bottom_bar)
+    }
+}
