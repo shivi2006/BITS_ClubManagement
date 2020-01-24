d0f5be8cb655f86929fb3e661c088bec41bb9619
shivi2006 <f20190778@pilani.bits-pilani.ac.in>
2020-01-24 11:13:23 +0530

    fragment

---
+++ app/src/main/java/com/example/shiviMittal/Attendance_class.kt
@@ -0,0 +1,19 @@
+package com.example.shiviMittal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Attendance_class:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?= inflater.inflate(R.layout.expenditure_layout,container,false)
+companion object{
+    fun newInstance():Attendance_class=Attendance_class()
+}
+
+}
