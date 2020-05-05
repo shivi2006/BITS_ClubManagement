963187d87e78e0c028a023a68c9fe841bca9688d
shivi2006 <f20190778@pilani.bits-pilani.ac.in>
2020-05-05 10:38:21 +0530

    Changed layout files

--- app/src/main/java/com/example/shiviMittal/Chat/View/AdapterChatView.kt
+++ app/src/main/java/com/example/shiviMittal/Chat/View/AdapterChatView.kt
@@ -14,13 +14,13 @@ class AdapterChatView():RecyclerView.Adapter<AdapterChatView.ViewHolder>() {
     var list:MutableList<ChatRoom> = ArrayList()
 
     override fun onCreateViewHolder(p: ViewGroup, viewType: Int): ViewHolder {
-        val view= LayoutInflater.from(p.context).inflate(R.layout.message,p,false)
+        val view= LayoutInflater.from(p.context).inflate(R.layout.chat_layout_row,p,false)
         return ViewHolder(view) // message layout left
     }
 
     override fun onBindViewHolder(p: ViewHolder, position: Int) {
         p.message_text.text= list[position].msg
-        p.message_time.text= list[position].time
+       // p.message_time.text= list[position].time
     }
 
     override fun getItemCount(): Int {
@@ -34,7 +34,7 @@ class AdapterChatView():RecyclerView.Adapter<AdapterChatView.ViewHolder>() {
 
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
 
-        val message_time= itemView.findViewById(R.id.message_time) as TextView
+       // val message_time= itemView.findViewById(R.id.message_time) as TextView
         val message_text= itemView.findViewById(R.id.message_text) as TextView
     }
 }
